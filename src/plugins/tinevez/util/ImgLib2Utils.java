5c5058b091d6b0f603a6c316872208fa708d30fb
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-02-27 16:31:27 +0100

    Utility class to run ImgLib2 examples.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/plugins/tinevez/util/ImgLib2Utils.java
@@ -0,0 +1,48 @@
+package plugins.tinevez.util;
+
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Util;
+
+
+public class ImgLib2Utils
+{
+	public static final < T extends RealType< T >> Img< FloatType > copyToFloat( final IterableInterval< T > source )
+	{
+		final ImgFactory< FloatType > factory = Util.getArrayOrCellImgFactory( source, new FloatType() );
+		final Img< FloatType > target = factory.create( source, new FloatType() );
+
+		if ( source.iterationOrder().equals( target.iterationOrder() ) )
+		{
+			final Cursor< T > sourceCursor = source.cursor();
+			final Cursor< FloatType > targetCursor = target.cursor();
+			while ( sourceCursor.hasNext() )
+			{
+				sourceCursor.fwd();
+				targetCursor.fwd();
+				targetCursor.get().set( sourceCursor.get().getRealFloat() );
+			}
+		}
+		else
+		{
+			final Cursor< T > sourceCursor = source.cursor();
+			final RandomAccess< FloatType > access = target.randomAccess( target );
+			while ( sourceCursor.hasNext() )
+			{
+				sourceCursor.fwd();
+				access.setPosition( sourceCursor );
+				access.get().set( sourceCursor.get().getRealFloat() );
+			}
+		}
+		return target;
+	}
+
+	private ImgLib2Utils()
+	{}
+
+}
